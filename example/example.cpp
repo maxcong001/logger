82fbf60451f5a4f872d040086b8c9fcccea245b7
savagecm <savagecm@qq.com>
2017-05-18 14:18:14 +0000

    add example

--- example/example.cpp
+++ example/example.cpp
@@ -3,5 +3,8 @@
 
 int main()
 {
-    __LOG(error, "hello logger!" << "test ostream");
+    __LOG(error, "hello logger!" << " error log");
+    __LOG(warn, "hello logger!" << " warn log");
+    __LOG(info, "hello logger!" << " info log");
+    __LOG(debug, "hello logger!" << " debug log");
 }
